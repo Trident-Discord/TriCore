b8f4efb3bb9afc714125f9a7393a3961cafbc010
Damian Freeman <49081789+iheartmemphis@users.noreply.github.com>
2019-03-30 17:59:30 -0400

    Added guild settings.

---
+++ Public Core/guild.js
@@ -0,0 +1,12 @@
+const GuildSettings = {
+    prefix: "+",
+    modLogChannel: "mod-log",
+    modRole: "Moderator",
+    adminRole: "Administrator",
+    systemNotice: "true",
+    welcomeChannel: "meet-and-greet",
+    welcomeMessage: "**{user}** has just joined.",
+    welcomeEnabled: "false"
+};
+
+module.exports = GuildSettings;
